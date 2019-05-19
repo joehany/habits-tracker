d3ca807325361f2acdf7e1dc67aac34d5f0e9e43
Sovann <seung.sovann@gmail.com>
2019-05-19 18:16:21 -0500

    finish UI

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttptrackerService } from '../httptracker.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(public myHttp: HttptrackerService) {
+
+  }
 
   ngOnInit() {
+
+  }
+
+  // Must refresh data
+  onChange(deviceValue) {
+    this.myHttp.getData('').subscribe(
+      res => {
+        console.log(res);
+        //console.log(JSON.parse(res));
+      } ,
+      err => console.log(err)
+    );
   }
 
 }
