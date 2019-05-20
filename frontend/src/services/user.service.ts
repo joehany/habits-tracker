c6516b0e4bfda754f61897653626df9b5fff61c8
TuyTam <doantamcdth5c@gmail.com>
2019-05-20 18:29:32 -0500

    Remove the unneeded code

--- frontend/src/services/user.service.ts
+++ frontend/src/services/user.service.ts
@@ -3,7 +3,6 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { map, catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 import { environment } from '../environments/environment';
-import { Duplex } from 'stream';
 
 @Injectable({
   providedIn: 'root'
