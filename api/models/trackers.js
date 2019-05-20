be48308bec13e4fac0a83efd72f104b85448a0b2
Joseph Hany <joe.hany@hotmail.com>
2019-05-20 11:16:03 -0500

    calculate tracker stats

--- api/models/trackers.js
+++ api/models/trackers.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const trackerUtils = require('../utils/trackers');
 
 const tracker = new mongoose.Schema({
   user: { type: String, required: true, index: true },
@@ -9,5 +10,16 @@ const tracker = new mongoose.Schema({
   daysPerWeek: { type: Number, min:1, max:7 },
   checkins: [Date],
 });
+
+tracker.virtual('stats').get(function() {
+  return {
+    daily: trackerUtils.calcDailyStats(this),
+    weekly: trackerUtils.calcWeeklyStats(this),
+    monthly: trackerUtils.calcMonhtlyStats(this),
+    yearly: trackerUtils.calcYearlyStats(this)
+  };
+});
+
+tracker.set('toJSON', { virtuals: true });
 
 module.exports = mongoose.model('Tracker', tracker);
