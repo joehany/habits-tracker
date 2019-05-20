7fc19fe18d46a5f7e4596f377aab2abb1f0c6e39
Sovann <seung.sovann@gmail.com>
2019-05-20 11:18:03 -0500

    apply api

--- frontend/src/app/detail/detail.component.ts
+++ frontend/src/app/detail/detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttptrackerService } from '../httptracker.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detail',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail.component.css']
 })
 export class DetailComponent implements OnInit {
+  showMsg: boolean = false;
+  _id: any;
 
-  constructor() { }
+  constructor(public myHttp: HttptrackerService, public route: ActivatedRoute) {
+    route.params.subscribe(params => {
+      this._id = params['id'];
+
+      // fake id
+      this._id = '5ce20ac2a3a1d755144d1dd5';
+    });
+  }
 
   ngOnInit() {
+
+  }
+
+  onCheckin() {
+  this.myHttp.postData('/' + this._id + '/checkin').subscribe(
+        res => {
+          this.showMsg = true;
+          setTimeout(() => {
+            this.showMsg = false;
+          }, 3000);
+        } ,
+        err => console.log(err)
+      );
   }
 
 }
--- frontend/src/app/httptracker.service.ts
+++ frontend/src/app/httptracker.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -7,31 +7,48 @@ import { HttpClient } from '@angular/common/http';
 // https://alligator.io/js/introduction-localstorage-sessionstorage/   ---> link for localstorage
 export class HttptrackerService {
   servername: string;
+  currentuser: any;
   token: string;
   myheader: any;
 
   constructor(public http: HttpClient) {
-    this.servername = 'https://randomuser.me/api';
+    this.servername = 'http://localhost:3000/trackers';
 
-    this.token = localStorage.getItem('token');
+    this.currentuser = {
+      _id: '5ce1e6a8a3a1d755144d1dd3',
+      email: 'seung.sovann@gmail.com',
+      name: 'Sovann',
+      createdAt: '2019-05-19T23:28:40.439Z',
+      token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNldW5nLnNvdmFubkBnbWFpbC5jb20iLCJpZCI6IjVjZTFlNmE4YTNhMWQ3NTUxNDRkMWRkMyIsImV4cCI6MTU2NjA4NDUyMCwiaWF0IjoxNTU4MzA4NTIwfQ.KxzRNLzyo6jp6ldts9CEriKpA34oyBTj5YhB3zwFKiI'
+    }
+    localStorage.setItem('currentuser', JSON.stringify(this.currentuser));
 
-    this.myheader = new Headers();
-    this.myheader.append('Content-Type', 'applicatoin/json');
-    //this.myheader.append('Authorizatoin', this.token);
+    this.token = JSON.parse(localStorage.getItem('currentuser')).token;
+
+    // this.myheader = new Headers();
+    // this.myheader.append('Content-Type', 'application/json');
+    // this.myheader.append('Authorization', 'Token ' + this.token);
+
+    this.myheader = new HttpHeaders().set('Authorization', 'Token ' + this.token);
   }
 
   // General http get
   getData(par: string) {
-    return this.http.get(this.servername + par, this.myheader);
+    return this.http.get(this.servername + par, {headers: this.myheader});
   }
 
   // General http post
-  postData(par: string, body?, header?) {
-    return this.http.post(this.servername + par, body, header);
+  postData(par: string, body?) {
+    return this.http.post(this.servername + par, body, {headers: this.myheader});
   }
 
-   // Get dashboard data
-   getDashboard() {
-    return this.http.get(this.servername);
+  // General http put
+  putData(par: string, body) {
+    return this.http.put(this.servername + par, body, {headers: this.myheader});
+  }
+
+  // General http delete
+  deleteData(par: string) {
+    return this.http.delete(this.servername + par, {headers: this.myheader});
   }
 }
