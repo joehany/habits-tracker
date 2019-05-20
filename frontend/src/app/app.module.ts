31bb5e8daa9b682e3c8e7047d9695999ac54bfee
TuyTam <doantamcdth5c@gmail.com>
2019-05-20 17:03:57 -0500

    Add Authen Guard

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,15 +8,13 @@ import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
 import { SigninComponent } from './signin/signin.component';
 import { HttpClientModule } from '@angular/common/http';
-import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SignupComponent,
-    SigninComponent,
-    LogoutComponent
+    SigninComponent
   ],
   imports: [
     BrowserModule,
