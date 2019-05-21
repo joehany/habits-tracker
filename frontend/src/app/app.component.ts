07bb1588b7ae2529d3b925996009e854f5d76344
Joseph Hany <joe.hany@hotmail.com>
2019-05-21 01:52:24 -0500

    added logout

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { ToastrService } from 'ngx-toastr';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.currentUser = this.userService.getCurrentUser();
+  }
+  currentUser: any;
+  constructor(private userService: UserService, private toastr: ToastrService ){}
+  logout() {
+    this.userService.logout();
+    this.toastr.success('Logged out successfully!');
+    window.location.reload();
+  }
 }
