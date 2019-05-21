989c51e4a4cecb2f2504cfeee46c69d2fba29622
Sovann <seung.sovann@gmail.com>
2019-05-21 14:00:43 -0500

    update folder structure

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { TrackerComponent  } from './tracker/tracker.component';
 import { TrackereditComponent } from './trackeredit/trackeredit.component';
 import { DetailComponent } from './detail/detail.component';
-import { AuthGuard } from 'src/guards/auth.guard';
+import { AuthGuard } from 'src/app/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent , canActivate: [AuthGuard]},
