b466e7c9f855411767c196c72f99a809beff411e
Joseph Hany <joe.hany@hotmail.com>
2019-05-21 22:07:54 -0500

    inject auth interceptor

--- frontend/src/app/services/httptracker.service.ts
+++ frontend/src/app/services/httptracker.service.ts
@@ -15,23 +15,6 @@ export class HttptrackerService {
 
   constructor(public http: HttpClient) {
     this.servername = environment.apiUrl + '/trackers';
-
-    // this.currentuser = {
-    //   _id: '5ce1e6a8a3a1d755144d1dd3',
-    //   email: 'seung.sovann@gmail.com',
-    //   name: 'Sovann',
-    //   createdAt: '2019-05-19T23:28:40.439Z',
-    //   token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNldW5nLnNvdmFubkBnbWFpbC5jb20iLCJpZCI6IjVjZTFlNmE4YTNhMWQ3NTUxNDRkMWRkMyIsImV4cCI6MTU2NjA4NDUyMCwiaWF0IjoxNTU4MzA4NTIwfQ.KxzRNLzyo6jp6ldts9CEriKpA34oyBTj5YhB3zwFKiI'
-    // }
-    // localStorage.setItem('currentuser', JSON.stringify(this.currentuser));
-
-    this.token = JSON.parse(localStorage.getItem('currentUser')).token;
-
-    // this.myheader = new Headers();
-    // this.myheader.append('Content-Type', 'application/json');
-    // this.myheader.append('Authorization', 'Token ' + this.token);
-
-    this.myheader = new HttpHeaders().set('Authorization', 'Token ' + this.token);
   }
 
   // General http get
